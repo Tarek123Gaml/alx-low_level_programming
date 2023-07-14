60bcec7a0d44f688073f013c1ac36163a53932b8
tarek <taga61188@gmail.com>
2023-07-14 11:39:34 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Retuen: Always 0 (Seccess)
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
