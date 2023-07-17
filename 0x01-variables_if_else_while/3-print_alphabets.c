575b9070b8e6f4d28b84a24b32314d89f0353fa7
tarek <taga61188@gmail.com>
2023-07-17 03:33:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: print all alphapet
+ *
+ * Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n')
+
+	return (0);
+}
