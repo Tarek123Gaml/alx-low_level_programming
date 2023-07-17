2c438b65cb6b308c5224d56acc434addd9e80e95
tarek <taga61188@gmail.com>
2023-07-17 13:03:42 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  * Description: print a quo using write function
  *	write(int fd,const void *buf,size_t count);
  *
- * Return: 1 (not success)
+ * Return: 1 (error)
 */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(1, quo, 59);
+	write(1, quo, 59);
 	return (1);
 }
