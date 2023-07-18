1f68466fc404ca3577da53bcac7b11f0060bd6cd
tarek <taga61188@gmail.com>
2023-07-18 11:38:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphapet - utilize on the _putchar function to print
+ * the alphapet a to z
+ *
+ * Return: print a to z
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; vh <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
