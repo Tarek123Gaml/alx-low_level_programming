9310c32072995991610a80d39c177b5648dabefe
tarek <taga61188@gmail.com>
2023-07-18 14:44:23 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * print_last_digit - check the code
+ *
+ * @n: takes inputs
+ *
+ * Return: Always 0.
+*/
+
+int print_last_digit(int n)
+{
+	printf(n % 10);
+
+	return (0);
+}
