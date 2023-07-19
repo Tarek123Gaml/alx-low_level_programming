07ae2a29c15a4fe2d1bfa363dd4e9d7c6f46caaf
tarek <taga61188@gmail.com>
2023-07-19 04:10:11 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * natural_c - check the code.
+ *
+ * Return: Always 0.
+*/
+
+void natural_c(void)
+{
+	int a, sum;
+
+	for (a = 1; 1 < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+			sum += a;
+	}
+	printf(sum);
+}
