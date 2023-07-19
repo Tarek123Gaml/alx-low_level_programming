d875e29ce1447684f95378656c3bca85d3130d8f
tarek <taga61188@gmail.com>
2023-07-19 01:42:01 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ *
+ * @n: takes input
+ * @m: takes input
+ *
+ * Return: Always 0.
+*/
+
+void add(int n, int m)
+{
+	int a = n + m;
+
+	return (a);
+}
