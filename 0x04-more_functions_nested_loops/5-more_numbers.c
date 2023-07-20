8b9aa026c3a225737ccad6b89c0b0219a1ab511e
tarek <taga61188@gmail.com>
2023-07-20 11:24:02 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,23 @@
-#include "
+#include "main.h"
+
+/**
+ * more_numbers - print numbers order
+ *
+ * Return: 0
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
