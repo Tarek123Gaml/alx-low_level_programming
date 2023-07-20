d763c7b0ff7b89bd26b96db46b44c51f7fc9250e
tarek <taga61188@gmail.com>
2023-07-20 09:03:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+*/
+
+oid print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		_putchar(a + 48);
+	_putchar('\n');
+}
