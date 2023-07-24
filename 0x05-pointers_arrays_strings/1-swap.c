25c33fde7d7f04ef2e231dbf99b36b7c066cf144
tarek <taga61188@gmail.com>
2023-07-24 11:58:48 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,17 @@
 /**
  * swap_int - check the code
  *
+ * @a: takes input
+ *
+ * @b: takes input
+ *
  * Return: Always 0.
 */
 
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = a;
 	a = b;
 	b = a;
