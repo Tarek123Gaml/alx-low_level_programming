410a8a105d2dcdd29dc9d0486343217afbd40701
tarek <taga61188@gmail.com>
2023-07-24 12:12:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string
+ *
+ * @s: takes input
+ *
+ * Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s != '\0'; s++)
+		++count;
+
+	return (count);
+}
