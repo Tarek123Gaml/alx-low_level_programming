a1791100a27c7c95de92ea72157b03089d3ca250
tarek <taga61188@gmail.com>
2023-07-24 13:32:40 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ *
+ * @str: takes string as input
+ *
+ * Return: 0
+*/
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int n = 0;
+
+	while (str[l] != '\0')
+		l++;
+
+	i++;
+	for (i /= 2; str[i]; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
