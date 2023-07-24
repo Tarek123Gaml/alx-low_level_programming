9680cff716fb45f93d788da8f2a6d95fd89a620b
tarek <taga61188@gmail.com>
2023-07-24 13:44:59 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print elements of arrays
+ *
+ * @n: takes input
+ *
+ * @a: takes input
+ *
+ * Return: 0
+*/
+
+void print_array(int *a, int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+		_putchar(a[l]);
+
+	_putchar('\n');
+}
