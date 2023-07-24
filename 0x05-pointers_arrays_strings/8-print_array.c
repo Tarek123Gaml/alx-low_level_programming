d84c4bf704d229bb7a3d8ae06d36d887ff0be1f3
tarek <taga61188@gmail.com>
2023-07-24 13:56:45 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,9 +17,9 @@ void print_array(int *a, int n)
 	for (l = 0; l < n; l++)
 	{
 		if (l != (n - 1))
-			printf("%d, ", a[i]);
+			printf("%d, ", a[l]);
 		else
-			printf("%d", a[i]);
+			printf("%d", a[l]);
 	}
 	_putchar('\n');
 }
