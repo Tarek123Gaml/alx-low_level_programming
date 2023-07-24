b7543fa70727eb9a16de46b341661f16e80c5efa
tarek <taga61188@gmail.com>
2023-07-24 12:22:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ *
+ * @str: takes input
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
