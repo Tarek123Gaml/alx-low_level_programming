703d2055f858993bbedea9894b50ffc4fc122c7e
tarek <taga61188@gmail.com>
2023-07-28 11:32:30 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * islower - check char
+ *
+ * @c: takes input
+ *
+ * Return: 0
+*/
+
+int islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isdel - check input
+ *
+ * @c: takes input
+ *
+ * Return: 0
+*/
+
+int isdel(char c)
+{
+	int i;
+	char del[] = " \t\n,.;!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == del[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - check str
+ *
+ * @str: takes inputs
+ *
+ * Return: 0
+*/
+
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int founddel = 1;
+
+	while (*str)
+	{
+		if (isdel(*str))
+			founddel = 1;
+		else if (islower(*str) && founddel)
+		{
+			*str -= 32;
+			founddel = 0;
+		}
+		else
+			fouddel = 0;
+		str++;
+	}
+
+	return (ptr);
+}
