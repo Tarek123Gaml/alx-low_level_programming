b089d8e8ae5a9a6cc724526f928f84439dc9e201
tarek <taga61188@gmail.com>
2023-07-28 09:14:09 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - function return order
+ *
+ * @s1: takes input
+ * @s2: takes inputs
+ *
+ * Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int equal = 0;
+
+	while (*s1)
+	{
+		if (s1 != s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+
+	return (equal);
+}
