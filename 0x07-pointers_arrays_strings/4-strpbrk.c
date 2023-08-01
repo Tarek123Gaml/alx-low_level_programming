53749588fc59182a4e9fd05092c185493330a302
tarek <taga61188@gmail.com>
2023-08-01 14:21:37 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,11 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char p;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
