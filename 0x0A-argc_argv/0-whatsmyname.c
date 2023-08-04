c9342533c334f5a428156d64c25156a757f526aa
tarek <taga61188@gmail.com>
2023-08-04 05:34:39 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - point entry
+ *
+ * @argc: count of arguments
+ * @argv: values of arguments
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
