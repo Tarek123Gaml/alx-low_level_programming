5b6ce5f2def072f3620c16ea88c49215d7dc1c87
tarek <taga61188@gmail.com>
2023-08-04 05:58:56 -0700

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - point entry
+ *
+ * @argc: count of arguments
+ * @argv: values of arguments
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		print("%d\n",argv[1] + argv[2]);
+	}
+
+	return (0);
+}
