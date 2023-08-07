d8c03a9b1e0b97df6b8bb602b62ed6727219c392
tarek <taga61188@gmail.com>
2023-08-07 10:32:12 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main"
+
+/**
+ * _strdup - return array
+ *
+ * @str: takes array
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+		;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (0);
+
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+
+	return (m);
+}
