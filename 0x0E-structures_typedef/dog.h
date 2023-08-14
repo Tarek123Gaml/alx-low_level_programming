e8af56caf6f9aabd2634905e65f52c2f66005062
tarek <taga61188@gmail.com>
2023-08-14 03:56:37 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * strcut dog - a dog struct
+ * @name: name dog
+ * @owner: name owner
+ * @age: age dog
+ *
+ * Description: that function take name and owner and age for dog
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
