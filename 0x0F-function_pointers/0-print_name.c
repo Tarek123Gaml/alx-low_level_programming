9ab501580b8d9fd1da3f3c8fa73744f863779061
tarek <taga61188@gmail.com>
2023-08-16 13:24:53 -0700

    m

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,16 +2,14 @@
 
 /**
  * print_name - that function print name
- *
  * @name: takes input as string
  * @f: takes input as function
  *
  * Return: void
-*/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
 }
-
