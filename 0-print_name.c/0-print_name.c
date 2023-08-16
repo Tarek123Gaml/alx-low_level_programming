6257871c295606e6676e372708a5a33896d140b2
tarek <taga61188@gmail.com>
2023-08-16 13:05:47 -0700

    m

---
+++ 0-print_name.c/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - that function print name
+ *
+ * @name: takes input as string
+ * @f: takes input as function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
