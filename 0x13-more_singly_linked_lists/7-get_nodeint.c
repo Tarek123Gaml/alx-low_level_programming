e3863b018c541672a93a8bc9bd4455a20476582c
tarek <taga61188@gmail.com>
2023-08-28 08:49:01 -0700

    linked kist

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp = head;
-
 	unsigned int i = 0;
 
-	while (temp && i < index)
+	while (head != NULL)
 	{
+		if (n == index)
+			return (head);
 		head = head->next;
 		i++;
 	}
-	return (head ? head : NULL);
+	return (NULL);
 }
