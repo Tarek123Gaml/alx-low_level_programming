e8d3f0c8bcc98fccc375fb589c4a971b91a2c21c
tarek <taga61188@gmail.com>
2023-08-27 20:49:59 -0700

    print linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,16 +10,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	listin_t *ptr = NULL;
-
-	ptr = h;
-
-	while (ptr != NULL)
+	while (h)
 	{
-		printf("%d", ptr->n);
-		ptr = ptr->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
