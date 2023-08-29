3cb5a1767e3b840c993236d3b194cfbf9b983478
tarek <taga61188@gmail.com>
2023-08-28 21:20:07 -0700

    linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - function that delit postion
+ *
+ * @head: head of list
+ * @index: postion delited
+ *
+ * Return: -1 or 1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *previus = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	else if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		current = NULL;
+		return (1);
+	}
+	else
+	{
+		while (index != 0)
+		{
+			previus = current;
+			current = current->next;
+			index--;
+		}
+		previus->next = current->next;
+		free(current);
+		current = NULL;
+		return (1);
+	}
+}
