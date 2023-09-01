f2b06f8ac1d60650f9332a52eac136a316517583
tarek <taga61188@gmail.com>
2023-09-01 10:18:56 -0700

    m

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -15,12 +14,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	while (*b)
 	{
-		if (*b != '0' || *b != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-
 		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
