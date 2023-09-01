150914667136708bce4052108dfca6021fdd8e3c
tarek <taga61188@gmail.com>
2023-08-31 20:38:06 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,10 @@ unsigned int binary_to_uint(const char *b)
 
 	while (*b)
 	{
-		if (b != '0' || b != '1')
+		if (*b != '0' || *b != '1')
 			return (0);
 
-		num = num * 2 + (b++ - '0');
+		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
 }
