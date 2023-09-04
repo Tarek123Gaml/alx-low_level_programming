eab90c82420e8a132bf3155c3422fc2f7c469559
tarek <taga61188@gmail.com>
2023-09-04 10:26:55 -0700

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	new_file = open(filenam, O_WRONLY | O_CREAT | 0_TRUNC, S_IRUSR, S_IWUSR);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new_file == -1)
 		return (-1);
 
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	for (len = 0; text_content[len]; len++)
 		;
-	stat = write(new_file, text_contanet, len);
+	stat = write(new_file, text_content, len);
 	if (close(new_file) == -1)
 		return (-1);
 	return (stat == -1 ? -1 : 1);
