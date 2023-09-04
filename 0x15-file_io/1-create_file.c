a2e1e1eaa3990e9cdafdb13dc0b42fdcd3a50511
tarek <taga61188@gmail.com>
2023-09-04 10:18:15 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,34 @@
 #include "main.h"
 
-/***/
+/**
+ * create_file - function that creat file whith data
+ *
+ * @filename: file name to creat
+ * @text_content: data insetr
+ *
+ * Return: 1 or -1 in error
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, len = 0;
-	while(*text_content++)
-		len++;
+	int new_file, len, stat;
 
+	if (filename == NULL)
+		return (-1);
 
+	new_file = open(filenam, O_WRONLY | O_CREAT | 0_TRUNC, S_IRUSR, S_IWUSR);
+	if (new_file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+	for (len = 0; text_content[len]; len++)
+		;
+	stat = write(new_file, text_contanet, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (stat == -1 ? -1 : 1);
+}
