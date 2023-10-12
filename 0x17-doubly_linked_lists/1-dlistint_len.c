a64df446c92cf234e571576b60eac22e3badc14b
tarek <taga61188@gmail.com>
2023-10-12 04:29:38 -0700

    linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-/***/
+/**
+ * dlistint_len - function with one argument
+ * @h: pointer to doubly linked list
+ *
+ * Description: count number of elements in doubly
+ * Return: total number of elements
+ */
 
 size_t dlistint_len(const dlistint_t *h)
 {
