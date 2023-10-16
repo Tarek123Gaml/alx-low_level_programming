b47a8b6fad41df8a6bf94801d1995ab7d24d5bd2
tarek <taga61188@gmail.com>
2023-10-16 11:16:42 -0700

    dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://https://github.com/Tarek123Gaml/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://https://github.com/Tarek123Gaml/alx-low_level_programming/blob/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
